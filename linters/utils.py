122a3ac38409ca3224c5fe96e359966bb1ba8159
Connor Boyle <connor.boyle@sagebase.org>
2020-07-21 16:55:32 -0700

    First Render Config Files as Template
    As far as I can tell, the sceptre stack config files (ending in ".yaml") are actually Jinja2 templates, that *produce valid YAML when rendered*. In order to get over all of the errors we were having, I set it to render the templates with relevant context variables set to blank strings, and then parse the generated config.

--- linters/utils.py
+++ linters/utils.py
@@ -1,11 +1,8 @@
 from yaml import load, FullLoader
+from jinja2 import Template
 from pathlib import Path
 
 
-# Let YAML handle tags naively
-def default_constructor(loader, tag_suffix, node):
-    return tag_suffix + ' ' + node.value
-FullLoader.add_multi_constructor('', default_constructor)
 
 
 def load_config(config_path):
@@ -14,8 +11,16 @@ def load_config(config_path):
     :param config_path: (str) path to config file, can be absolute or relative to working directory
     :return: Python object representing structure of config file
     """
+
+    # Let YAML handle tags naively
+    def default_constructor(loader, tag_suffix, node):
+        return tag_suffix + ' ' + node.value
+    FullLoader.add_multi_constructor('', default_constructor)
+
     with open(config_path) as new_file:
-        return load(new_file, Loader=FullLoader)
+        # Load template with blanks for all variables
+        template = Template(new_file.read())
+        return load(template.render(stack_group_config=''), Loader=FullLoader)
 
 
 def is_subpath(parent_path: str, child_path: str):
